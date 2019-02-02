dad40795455790efbc84af8a1c9d634cb8e59420
Ali Groening <allison.groening@sapientrazorfish.com>
2019-02-01 19:26:54 -0800

    [vid-14] updated our data model, created actual mutations and queries and tested them

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,24 @@
 const Mutations = {
-    createDog( parent, args, ctx, info ) {
-        // Create a dog
-        global.dogs = global.dogs || [];
-        const newDog = { name: args.name };
-        global.dogs.push( newDog );
-        // console.log( 'args', args );
-        return newDog;
+    // createDog( parent, args, ctx, info ) {
+    //     // Create a dog
+    //     global.dogs = global.dogs || [];
+    //     const newDog = { name: args.name };
+    //     global.dogs.push( newDog );
+    //     // console.log( 'args', args );
+    //     return newDog;
+    // }
+    async createItem( parent, args, ctx, info ) {
+
+        // TODO: check if they are logged in
+
+        // all these functions from our database return promises
+        const item = await ctx.db.mutation.createItem( {
+            data: {
+                ...args
+            }
+        }, info );
+
+        return item;
     }
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,10 +1,14 @@
+const { forwardTo } = require( 'prisma-binding' );
+
 const Query = {
 
-    dogs( parent, args, ctx, info ) {
-        global.dogs = global.dogs || [];
-        return global.dogs;
-        // return [ { name: 'Oliver' }, { name: 'Salty' } ]
-    }
+    items: forwardTo( 'db' )
+    // async items( parent, args, ctx, info ) {
+    //     const items = await ctx.db.query.items ();
+
+    //     return items;
+
+    // }
 };
 
 module.exports = Query;
