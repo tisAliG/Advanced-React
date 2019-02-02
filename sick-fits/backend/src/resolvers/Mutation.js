068382ef7b5def1947a4123f98c5397911463672
Ali Groening <allison.groening@sapientrazorfish.com>
2019-02-01 17:44:57 -0800

    [vid-13] playing around with queries and mutations within yoga, these are just tests and will get removed

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+    createDog( parent, args, ctx, info ) {
+        // Create a dog
+        global.dogs = global.dogs || [];
+        const newDog = { name: args.name };
+        global.dogs.push( newDog );
+        // console.log( 'args', args );
+        return newDog;
+    }
+};
 
 module.exports = Mutations;
