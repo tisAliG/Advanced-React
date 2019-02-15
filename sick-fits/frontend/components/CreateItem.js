7094b67b0d7058de2db03ff04de9d64ad4b4768c
Ali Groening <allison.groening@sapientrazorfish.com>
2019-02-14 17:06:24 -0800

    [vid-18] created a cloudinary account and some upload presets, then on the create page use that service to upload our users images to

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -68,6 +68,30 @@ class CreateItem extends Component {
 
     };
 
+    uploadFile = async ( event ) => {
+
+        console.log( 'uploading file..' );
+        const files = event.target.files;
+        const data = new FormData();
+
+        data.append( 'file', files[ 0 ] );
+        data.append( 'upload_preset', 'sickfits' );
+
+        const res = await fetch( 'https://api.cloudinary.com/v1_1/tisalig/image/upload', {
+            method: 'POST',
+            body: data
+        } );
+
+        const file = await res.json();
+
+        console.log( 'FILE', file );
+
+        this.setState( {
+            image: file.secure_url,
+            largeImage: file.eager[ 0 ].secure_url
+        } );
+    };
+
     render() {
         return (
             <Mutation
@@ -79,37 +103,59 @@ class CreateItem extends Component {
                         <Form onSubmit={ ( event ) => this.handleSubmit( event, createItem ) }>
                             <Error error={ error } />
                             <fieldset disabled={ loading } aria-busy={ loading }>
-                                <label htmlFor='title'>Title</label>
-                                <input
-                                    type='text'
-                                    id='title'
-                                    name='title'
-                                    placeholder='title'
-                                    value={ this.state.title }
-                                    onChange={ this.handleChange }
-                                    required
-                                />
+                                <label htmlFor='file'>
+                                    File
+                                    <input
+                                        type='file'
+                                        id='file'
+                                        name='file'
+                                        placeholder='file'
+                                        onChange={ this.uploadFile }
+                                        required
+                                    />
 
-                                <label htmlFor='description'>Description</label>
-                                <textarea
-                                    id='description'
-                                    name='description'
-                                    placeholder='Enter a Description'
-                                    value={ this.state.description }
-                                    onChange={ this.handleChange }
-                                    required
-                                />
+                                    { this.state.image &&
+                                        <img src={ this.state.image } alt='upload preview' />
+                                    }
+                                </label>
 
-                                <label htmlFor='price'>Price</label>
-                                <input
-                                    type='number'
-                                    id='price'
-                                    name='price'
-                                    placeholder={ 0 }
-                                    value={ this.state.price }
-                                    onChange={ this.handleChange }
-                                    required
-                                />
+                                <label htmlFor='title'>
+                                    Title
+                                    <input
+                                        type='text'
+                                        id='title'
+                                        name='title'
+                                        placeholder='title'
+                                        value={ this.state.title }
+                                        onChange={ this.handleChange }
+                                        required
+                                    />
+                                </label>
+
+                                <label htmlFor='description'>
+                                    Description
+                                    <textarea
+                                        id='description'
+                                        name='description'
+                                        placeholder='Enter a Description'
+                                        value={ this.state.description }
+                                        onChange={ this.handleChange }
+                                        required
+                                    />
+                                </label>
+
+                                <label htmlFor='price'>
+                                    Price
+                                    <input
+                                        type='number'
+                                        id='price'
+                                        name='price'
+                                        placeholder={ 0 }
+                                        value={ this.state.price }
+                                        onChange={ this.handleChange }
+                                        required
+                                    />
+                                </label>
 
                                 <button type='submit'>Submit</button>
                             </fieldset>
