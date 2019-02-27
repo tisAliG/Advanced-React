00e420ba71879f61a45423db39c816eb73e6209c
Ali Groening <allison.groening@sapientrazorfish.com>
2019-02-26 19:48:58 -0800

    [vid-25] created markup for user sign up form and handled the sign up mutation on the frontend

---
+++ sick-fits/frontend/pages/signup.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import Signup from '../components/signup';
+
+const Columns = styled.div`
+    display: grid;
+    grid-template-columns: repeat( auto-fit, minmax( 300px, 1fr ) );
+    grid-gap: 20px;
+`;
+
+const signupPage = ( props ) => (
+    <Columns>
+        <Signup />
+        <Signup />
+        <Signup />
+    </Columns>
+);
+
+export default signupPage;
